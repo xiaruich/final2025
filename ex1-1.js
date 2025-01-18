fee8d2eaac6cb8a4895c847fad67413573d825c6
xiaruich <168495529+xiaruich@users.noreply.github.com>
2025-01-18 23:18:43 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,7 +1,6 @@
-function hyoji() {
-var str = "Welcome the magic world!";
-var cnt = document.timer.moji.value.length;
-if ( cnt < 8 ) {
-document.timer.moji.value = str.substr(0,cnt+1); } else {
-document.timer.moji.value = ""; }
-setTimeout("hyoji()",500); }
+function imgchange1() {
+    document.getElementById('monstar').src = './image/2.jpg';
+}
+function imgchange2() {
+    document.getElementById('monstar').src = './image/1.jpg';
+}
