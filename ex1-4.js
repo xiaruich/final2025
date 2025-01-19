fbcd67e63dafec5ee325aed2ba32403c03531cbf
xiaruich <168495529+xiaruich@users.noreply.github.com>
2025-01-19 20:33:52 +0900

    Update ex1-4.js

--- ex1-4.js
+++ ex1-4.js
@@ -1,2 +1,4 @@
-function nazo2(){if(document.getElementById('answer2').value=='エンシェント・ワン'){alert('あたり!');}
-                else{alert('はずれ');}}
+function nazo2()
+{if(document.getElementById('answer2').value=='エンシェント・ワン')
+{alert('あたり!');}
+　else{alert('はずれ');}}
